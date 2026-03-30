2fe27eb1efa17ea175578e31c7e3da21da97612c
Adekunle Bamzz <68190511+AdekunleBamz@users.noreply.github.com>
2026-03-30 06:36:05 +0100

    Add explicit function signature for async handler

--- server/src/middleware/errorHandler.ts
+++ server/src/middleware/errorHandler.ts
@@ -58,7 +58,7 @@ export function errorHandler(
  */
 export function asyncHandler(
   fn: (req: Request, res: Response, next: NextFunction) => Promise<unknown>
-) {
+): (req: Request, res: Response, next: NextFunction) => void {
   return (req: Request, res: Response, next: NextFunction) => {
     Promise.resolve(fn(req, res, next)).catch(next);
   };
