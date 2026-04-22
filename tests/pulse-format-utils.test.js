9756b19f70fd99b5e6caf01d10a2b2c3114b192c
Adekunle Bamzz <68190511+AdekunleBamz@users.noreply.github.com>
2026-04-22 22:28:44 +0100

    Cover pulse volume formatting

--- tests/pulse-format-utils.test.js
+++ tests/pulse-format-utils.test.js
@@ -44,4 +44,8 @@ describe('pulse format utils', () => {
   it('formats pulse addresses with visible ends', () => {
     expect(formatPulseAddress('SP3K8BC0PPEVCV7NZ6QSRWPQ2JE9E5B6N3PA0KBR9')).toBe('SP3K8B...KBR9');
   });
+
+  it('formats pulse volumes in million STX units', () => {
+    expect(formatPulseVolume(2_500_000)).toBe('2.50M STX');
+  });
 });
